081e1564425b2f799f181402c2b06ce3fb3f7aa2
Isaac Blandin Mac <myung0966@gmail.com>
2019-02-11 11:42:56 -0600

    added opmodes without hanging

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckusV2/OpModes/Autonomous/BoxAutoNoLand.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode.RoverRuckusV2.OpModes.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.RoverRuckusTemp.Base.RobotRunType;
+import org.firstinspires.ftc.teamcode.RoverRuckusV2.Base.AutoType;
+import org.firstinspires.ftc.teamcode.RoverRuckusV2.Base.AutonomousBaseV2;
+
+/**
+ * Created by isaac.blandin on 2/11/19.
+ */
+
+@Autonomous(name = "box no land")
+public class BoxAutoNoLand extends AutonomousBaseV2{
+
+    public void runOpMode(){
+
+        //set up robot
+        initRobotV2(RobotRunType.AUTONOMOUS);
+        initVuforia();
+        initTfod();
+        //free motors for landing
+        armRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        armLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        //reset all encoders
+        resetEncoders();
+        resetArmEncoders();
+
+        waitForStart();
+
+        //sample minerals
+        sample(AutoType.DEPOT);
+        //drive to depot
+        waitSec(0.2);
+        drive(0.5, 31);
+        waitSec(0.2);
+        turnHeading(0.3, -45);
+        waitSec(0.2);
+        strafeRot(-0.5, 0.5);
+        strafeRot(0.3, 0.2);
+        waitSec(0.2);
+        turnHeading(0.15, -42);
+        drive(0.5, 55);
+        //drop team marker
+        collectorTime(1, -1);
+        //drive to crater to park
+        drive(-0.5, 30);
+        waitSec(0.2);
+        strafeRot(0.5, 0.5);
+        waitSec(0.2);
+        turnHeading(0.45, 135);
+        turnHeading(0.2, 135);
+        waitSec(0.2);
+        strafeRot(0.5, 0.5);
+        waitSec(0.2);
+        drive(0.5, 20);
+        waitSec(0.2);
+        //extend arm over crater
+        extendArm(10, 1);
+
+        stop();
+
+
+    }
+
+}
---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckusV2/OpModes/Autonomous/CraterAutoNoLand.java
@@ -0,0 +1,64 @@
+package org.firstinspires.ftc.teamcode.RoverRuckusV2.OpModes.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.RoverRuckusTemp.Base.RobotRunType;
+import org.firstinspires.ftc.teamcode.RoverRuckusV2.Base.AutoType;
+import org.firstinspires.ftc.teamcode.RoverRuckusV2.Base.AutonomousBaseV2;
+
+/**
+ * Created by isaac.blandin on 2/11/19.
+ */
+
+@Autonomous(name = "crater no land")
+public class CraterAutoNoLand extends AutonomousBaseV2 {
+
+    public void runOpMode(){
+
+        //set up robot
+        initRobotV2(RobotRunType.AUTONOMOUS);
+        initVuforia();
+        initTfod();
+        //free motors for landing
+        armRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        armLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        //reset all encoders
+        resetEncoders();
+        resetArmEncoders();
+
+        waitForStart();
+
+        //sample minerals
+        sample(AutoType.CRATER);
+        //drive to depot
+        waitSec(0.3);
+        drive(0.5, 32);
+        waitSec(0.3);
+        turnHeading(0.3, 135);
+        turnHeading(0.2, 135);
+        waitSec(0.3);
+        drive(0.5, 30);
+        waitSec(0.2);
+        //drop the team marker
+        collectorTime(1, -1);
+        //drive to crater to park
+        drive(-0.5, 30);
+        waitSec(0.3);
+        strafeRot(-0.5, 0.5);
+        waitSec(0.3);
+        turnHeading(0.45, -42);
+        turnHeading(0.2, -42);
+        waitSec(0.3);
+        strafeRot(-0.5, 0.5);
+        waitSec(0.3);
+        drive(0.5, 20);
+        waitSec(0.3);
+        //extend arm over crater
+        extendArm(10, 1);
+
+        stop();
+
+    }
+
+}
