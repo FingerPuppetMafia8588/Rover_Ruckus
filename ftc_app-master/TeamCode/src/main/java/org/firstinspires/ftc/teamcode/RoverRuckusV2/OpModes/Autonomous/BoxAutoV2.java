6082556258b027dbf5769c3dccec8e8244647fb5
Isaac Blandin Mac <myung0966@gmail.com>
2019-02-11 11:42:26 -0600

    cleaned up program

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckusV2/OpModes/Autonomous/BoxAutoV2.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckusV2/OpModes/Autonomous/BoxAutoV2.java
@@ -1,67 +1,39 @@
 package org.firstinspires.ftc.teamcode.RoverRuckusV2.OpModes.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.RoverRuckusTemp.Base.RobotRunType;
+import org.firstinspires.ftc.teamcode.RoverRuckusV2.Base.AutoType;
 import org.firstinspires.ftc.teamcode.RoverRuckusV2.Base.AutonomousBaseV2;
 
 /**
  * Created by isaac.blandin on 1/21/19.
  */
 
-@Autonomous(name = "box V2")
+@Autonomous(name = "box full")
 public class BoxAutoV2 extends AutonomousBaseV2{
 
     public void runOpMode(){
 
+        //set up robot
         initRobotV2(RobotRunType.AUTONOMOUS);
         initVuforia();
         initTfod();
-
+        //free motors for landing
+        armRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        armLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        //reset all encoders
         resetEncoders();
         resetArmEncoders();
 
         waitForStart();
 
-        drive(0.5, 7.5  );
-        turnHeading(0.25, 90);
-        turnHeading(0.15, 92);
-        drive(-0.3, 3.9);
-        waitSec(1);
-        getGoldPos();
-        waitSec(0.3);
-
-        if (gold_position == GOLD_POSITION.RIGHT){
-            drive(-0.3, 10);
-            waitSec(0.2);
-            strafeRot(0.5, 1.5);
-            strafeRot(-0.5, 1.5);
-            turnHeading(0.3, 90);
-            turnHeading(0.2, 90);
-            waitSec(0.3);
-            drive(0.3, 27);
-
-
-        } else if (gold_position == GOLD_POSITION.CENTER){
-            drive(0.3, 5);
-            waitSec(0.2);
-            strafeRot(0.5, 1.5);
-            strafeRot(-0.5,1.5 );
-            turnHeading(0.3, 90);
-            turnHeading(0.2, 90);
-            waitSec(0.3);
-            drive(0.3, 12);
-
-        } else {
-            drive(0.3, 17);
-            waitSec(0.2);
-            strafeRot(0.5, 1.5);
-            strafeRot(-0.5, 1.5);
-            turnHeading(0.3, 90);
-            turnHeading(0.15, 90);
-
-        }
-
+        //land robot
+        land();
+        //sample minerals
+        sample(AutoType.DEPOT);
+        //drive to depot
         waitSec(0.2);
         drive(0.5, 31);
         waitSec(0.2);
@@ -71,10 +43,10 @@ public class BoxAutoV2 extends AutonomousBaseV2{
         strafeRot(0.3, 0.2);
         waitSec(0.2);
         turnHeading(0.15, -42);
-
         drive(0.5, 55);
+        //drop team marker
         collectorTime(1, -1);
-
+        //drive to crater to park
         drive(-0.5, 30);
         waitSec(0.2);
         strafeRot(0.5, 0.5);
@@ -86,6 +58,7 @@ public class BoxAutoV2 extends AutonomousBaseV2{
         waitSec(0.2);
         drive(0.5, 20);
         waitSec(0.2);
+        //extend arm over crater
         extendArm(10, 1);
 
         stop();
--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckusV2/OpModes/Autonomous/CraterAutoV2.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckusV2/OpModes/Autonomous/CraterAutoV2.java
@@ -4,65 +4,36 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.RoverRuckusTemp.Base.RobotRunType;
+import org.firstinspires.ftc.teamcode.RoverRuckusV2.Base.AutoType;
 import org.firstinspires.ftc.teamcode.RoverRuckusV2.Base.AutonomousBaseV2;
 
 /**
  * Created by isaac.blandin on 1/21/19.
  */
 
-@Autonomous(name = "crater V2")
+@Autonomous(name = "crater full")
 public class CraterAutoV2 extends AutonomousBaseV2 {
 
     public void runOpMode(){
 
+        //set up robot
         initRobotV2(RobotRunType.AUTONOMOUS);
         initVuforia();
         initTfod();
-
+        //free motors for landing
+        armRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        armLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        //reset all encoders
         resetEncoders();
         resetArmEncoders();
 
         waitForStart();
 
-        drive(0.5, 9.5);
-        turnHeading(0.25, 90);
-        turnHeading(0.15, 90);
-        drive(-0.3, 3.9);
-        waitSec(1);
-        getGoldPos();
-        waitSec(0.3);
-
-        if (gold_position == GOLD_POSITION.RIGHT){
-            drive(-0.3, 10);
-            waitSec(0.5);
-            strafeRot(0.5, 1.5);
-            strafeRot(-0.5, 1);
-            turnHeading(0.3, 90);
-            turnHeading(0.2, 90);
-            waitSec(0.3);
-            drive(0.3, 27);
-
-
-        } else if (gold_position == GOLD_POSITION.CENTER){
-            drive(0.3, 5);
-            waitSec(0.5);
-            strafeRot(0.5, 1.5);
-            strafeRot(-0.5,1 );
-            turnHeading(0.3, 90);
-            turnHeading(0.2, 90);
-            waitSec(0.3);
-            drive(0.3, 12);
-
-        } else {
-            drive(0.3, 17);
-            waitSec(0.5);
-            strafeRot(0.5, 1.5);
-            strafeRot(-0.5, 1);
-            turnHeading(0.3, 90);
-            turnHeading(0.2, 90);
-
-        }
-
+        //land robot
+        land();
+        //sample minerals
+        sample(AutoType.CRATER);
+        //drive to depot
         waitSec(0.3);
         drive(0.5, 32);
         waitSec(0.3);
@@ -71,7 +42,9 @@ public class CraterAutoV2 extends AutonomousBaseV2 {
         waitSec(0.3);
         drive(0.5, 30);
         waitSec(0.2);
+        //drop the team marker
         collectorTime(1, -1);
+        //drive to crater to park
         drive(-0.5, 30);
         waitSec(0.3);
         strafeRot(-0.5, 0.5);
@@ -83,6 +56,7 @@ public class CraterAutoV2 extends AutonomousBaseV2 {
         waitSec(0.3);
         drive(0.5, 20);
         waitSec(0.3);
+        //extend arm over crater
         extendArm(10, 1);
 
         stop();
